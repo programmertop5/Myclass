ba1349d2e03fe8895286142cd0556d4a1ebff7ee
programmertop5 <kardrkarina@gmail.com>
2025-09-14 19:45:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,31 @@ int main() {
     String s2("Hi");
     String s3("Programming");
 
+    s2 = s1; 
     s1.print();
     s2.print();
     s3.print();
 
-    s1.push_back('A'); 
+    s1.push_back('A');
     s1.push_back('B');
     s1.print();
-    s1.pop_back(); 
+    s1.pop_back();
     s1.print();
 
-    s1.append("Karate!"); 
+    s3 = s1; 
+    s1.append("Karate!");
     s1.print();
-    s1.assign("I like programming"); 
+    s1.assign("I like programming");
     s1.print();
 
-    cout << "s1.find('w'): " << s1.find('K') << endl;
-    cout << "s1.find('S'): " << s1.find('e') << endl;
-    cout << "s1.find('Z'): " << s1.find('x') << endl;
+    cout << "s1.find('K'): " << s1.find('K') << endl;
+    cout << "s1.find('e'): " << s1.find('e') << endl;
+    cout << "s1.find('x'): " << s1.find('x') << endl;
 
     String s4("Hello");
     s4.print();
-    s4.clear(); s4.print();
+    s4.clear();
+    s4.print();
 
     return 0;
 }
